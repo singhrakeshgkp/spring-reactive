74f7a737269f63003dc747db2863e20dc2bb00f7
Rakesh Singh <rakesh-sin@HCL.COM>
2023-04-03 23:00:30 -0700

    added reactive mongo application

---
+++ sreactive-mongo-integration/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.5"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+	implementation("org.springframework.boot:spring-boot-starter-rsocket")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ sreactive-mongo-integration/src/main/java/com/example/sreactivemongointegration/SreactiveMongoIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sreactivemongointegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreactiveMongoIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreactiveMongoIntegrationApplication.class, args);
+	}
+
+}
